5c8fbfe911f275efabea7900e7a0124528b078d8
kyle-sm <steven.kyle.97@gmail.com>
2021-01-02 13:38:46 -0500

    updated .gitignore and added some more options to main

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ import logging
 
 
 LOG_LEVEL_DICT = {
-    "CRITICAL" : logging.CRITICAL,
-    "ERROR" : logging.ERROR,
-    "WARNING" : logging.WARNING,
-    "INFO" : logging.INFO,
-    "DEBUG" : logging.DEBUG
+    "CRITICAL": logging.CRITICAL,
+    "ERROR": logging.ERROR,
+    "WARNING": logging.WARNING,
+    "INFO": logging.INFO,
+    "DEBUG": logging.DEBUG
 }
 
 
 def main():
     """The main method. Processes flags to determine how to run the program."""
     mainlogger = logging.getLogger(__name__)
+    battle_format = "gen8ou"
 
     # Read through flags and set variables accordingly
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "l:", ["loglevel="])
+        opts, args = getopt.getopt(sys.argv[1:], "l:f:h", ["loglevel=", "battleformat=", "help"])
     except getopt.GetoptError as err:
         print(err)
         sys.exit(2)
@@ -31,9 +32,17 @@ def main():
             try:
                 logging.basicConfig(level=LOG_LEVEL_DICT[arg])
             except KeyError as err:
-                print(f'Invalid log level passed to {opt}.')
-                print('Valid arguments are CRITICAL, ERROR, WARNING, INFO, and DEBUG.')
+                print(f'Invalid log level passed to {opt}.',
+                       '\nValid arguments are CRITICAL, ERROR,',
+                       'WARNING, INFO, and DEBUG.')
                 sys.exit(2)
+        elif opt in ('-f', '--battleformat'):
+            battle_format = arg;
+        elif opt in ('-h', '--help'):
+            print('-f, --battleformat=:\n\tSet battle format (default gen8ou)',
+                  '\n-l, --loglevel=:\n\tSet log level (default WARNING)',
+                  '\n-h, --help:\n\tDisplay help message')
+            sys.exit(2)
     return
 
 
