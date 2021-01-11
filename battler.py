08d766c3908888674ef79df6b900b5dab6388abb
kyle-sm <steven.kyle.97@gmail.com>
2021-01-11 18:13:42 -0500

    Implemented bad battle methods

--- battler.py
+++ battler.py
@@ -9,6 +9,7 @@ import requests
 import asyncio
 import json
 import logging
+import random
 
 class PSSock():
 
@@ -34,27 +35,44 @@ class PSSock():
                     'act': 'login',
                     'name': self.username,
                     'pass': self.password,
-                    'challstr': f'{challstr[1]}|{challstr[2]}'
+                    'challstr': f'{challstr[2]}|{challstr[3]}'
                 }
             )
             if response.status_code == 200:
                 response_json = json.loads(response.text[1:])
+                self.log.info('login request response:\n' + json.dumps(response_json, indent=3))
                 if not response_json['actionsuccess']:
                     raise RuntimeError(f'Error logging in.\n{response.content}')
                 assertion = response_json['assertion']
                 await ws.send(f'|/trn {self.username},0,{assertion}')
                 self.log.warn("Login successful.")
-                self.log.warn(json.dumps(response_json, indent=3))
             else:
                 raise RuntimeError(f'Error logging in.\n{response.content}')
+            while True:
+                challenge = await self.__wait_for_msg(ws, 'updatechallenges')
+                json_data = json.loads(challenge[2])
+                for username, format in json_data['challengesFrom'].items():
+                    await ws.send(f'|/utm null')
+                    await ws.send(f'|/accept {username}')
+                    initmsg = await self.__wait_for_msg(ws, 'init')
+                    await self.__battle_routine(ws, initmsg[0].rstrip())
 
-    async def __handle_response(self, response):
-        pass
-
-    async def __wait_for_msg(self, ws, msgtype):
+    async def __wait_for_msg(self, ws, msgtype='', room=''):
         while True:
             message = await ws.recv()
-            message = message.split("|")[1:]
-            if message[0] == msgtype:
-                return message
-        
+            message = message.split('|')
+            print(message)
+            if (msgtype == '' or message[1] == msgtype): 
+                if (room == '' or message[0] == room):
+                    return message
+
+    # the current implementation is a placeholder that does random action to
+    # test the connection. Once a proper training method is implemented, the AI
+    # will make proper, intelligent decisions
+    async def __battle_routine(self, ws, roomid):
+        print(roomid)
+        while True:
+            await ws.send(f'{roomid}|/choose move {random.randint(1,4)}')
+            await asyncio.sleep(20)
+            await ws.send(f'{roomid}|/switch {random.randint(1,6)}')
+
